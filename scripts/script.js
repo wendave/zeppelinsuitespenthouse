d7afea455301649ef3b7daf6af3e7c4d530e090e
owen <owendavecaisip@gmail.com>
2021-07-16 16:51:48 +0800

    Fixed Conflicts/Errors in WP

--- scripts/script.js
+++ scripts/script.js
@@ -57,12 +57,6 @@ var swipers = () => {
   var swiperFeatured = new Swiper(".featured-swiper", {
     // slidesPerView: 4,
     slidesPerGroup: 1,
-    // loop: true,
-    // loopFillGroupWithBlank: true,
-    // pagination: {
-    //   el: ".featured-swiper .swiper-pagination",
-    //   clickable: true,
-    // },
     navigation: {
       nextEl: ".featured-swiper .swiper-button-next-custom",
       prevEl: ".featured-swiper .swiper-button-prev-custom",
@@ -87,47 +81,49 @@ var swipers = () => {
 // feature section with swiper thumbnails
 var featured = () => {
   var featuredSection = document.getElementById("featured_section");
-  window.addEventListener("load", function() {
-    if (featuredSection) {
+  if (featuredSection) {
 
-      // on load, display the first thumbnail
-      setTimeout(() => {
-        $("#featured_section .swiper-slide-active .thumbnail-image").click();
-      }, 100);
+    // on load, display the first thumbnail
+    setTimeout(() => {
+      $("#featured_section .swiper-slide-active .thumbnail-image").click();
+    }, 100);
 
-      setTimeout(() => {
-        $("#featured_section").addClass("show");
-      }, 500);
-  
-      $(".featured-swiper .thumbnail-image").each(function() {
-        // $(this).click(function() {
-        //   var imageSrc = $(this).attr("src");
-  
-        //   $("#featured_wrapper").fadeOut();
-        //   setTimeout(() => {
-        //     $("#featured_image").attr("src", imageSrc);
-        //     $("#featured_wrapper").fadeIn();
-        //   }, 300);
-        // });
-  
-        $(this).click(function() {
-          var imageSrc = $(this).attr("src");
-  
-          if (!$(this).hasClass("active")) {
-            $(this).addClass("active");
-            $(".featured-swiper .thumbnail-image").removeClass("active");
-            $("#featured_wrapper").css("opacity","0");
-            
-            setTimeout(() => {
-              $("#featured_image").attr("src", imageSrc);
-              $("#featured_wrapper").css("opacity","1");
-            }, 500);
-            $(this).addClass("active");
-          }
-        });
+    setTimeout(() => {
+      $("#featured_section").addClass("show");
+    }, 500);
+
+    $(".featured-swiper .thumbnail-image").each(function() {
+      // $(this).click(function() {
+      //   var imageSrc = $(this).attr("src");
+
+      //   $("#featured_wrapper").fadeOut();
+      //   setTimeout(() => {
+      //     $("#featured_image").attr("src", imageSrc);
+      //     $("#featured_wrapper").fadeIn();
+      //   }, 300);
+      // });
+
+      $(this).click(function() {
+        var imageSrc = $(this).attr("src");
+        var imageDescription = $(this).parent().find(".thumbnail-description").text();
+
+        if (!$(this).hasClass("active")) {
+          $(this).addClass("active");
+          $(".featured-swiper .thumbnail-image").removeClass("active");
+          $("#featured_wrapper").css("opacity","0");
+          $(".featured-text").css("opacity","0");
+          
+          setTimeout(() => {
+            $("#featured_image").attr("src", imageSrc);
+            $("#featured_text p").text(imageDescription);
+            $("#featured_wrapper").css("opacity","1");
+            $(".featured-text").css("opacity","1");
+          }, 500);
+          $(this).addClass("active");
+        }
       });
-    }
-  });
+    });
+  }
 }
 
 // initialize the functions
